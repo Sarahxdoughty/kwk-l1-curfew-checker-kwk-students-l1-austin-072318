cbad5500f4a2466db1f0dcc0c1e27b5dc59d5303
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-24 17:00:58 +0000

    automatically backed up by learn

--- curfewChecker.rb
+++ curfewChecker.rb
@@ -1,8 +1,6 @@
 def simple_curfew_checker (time)
-  if time<=11
-    puts "you are not in trouble"
-  else
-    puts "you are in trouble"
+  if time>=11
+    puts "You're in trouble! Better get home quick!"
   end
 end
 
